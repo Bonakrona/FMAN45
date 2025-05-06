7df4bab2ead06b7b334021957f506954cbb9834d
Jona Waldfogel <jona.waldfogl@gmail.com>
2025-05-06 17:39:57 +0200

    E12 done. However need to put both pca.py and clustering.py in the main file because I did it wrong from the start. That will lead to a lot of refactoring though which is a good thing

--- Code stub and data (3)/Code stub (for students)/Python/K_means_clustering.py
+++ Code stub and data (3)/Code stub (for students)/Python/K_means_clustering.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy
+from sklearn import svm
+from sklearn.svm import SVC
 
 def K_means_clustering(X, K):
     """
@@ -59,8 +61,8 @@ def fcdist(C1,C2):
 
 def load_data():
     # Replace '/path/to/file/' with the path to your .mat file
-    base_path = "/path/to/file/"
-    mat_file_path = base_path + "A2_data.mat"
+    base_path = "C:/Users/User/OneDrive/Documents/Lund ar 5/vt/FMAN45/Assignments/1/FMAn45/Code stub and data (3)/Code stub (for students)/Python/A2_data.mat"
+    mat_file_path = base_path
     try:
         mat_data = scipy.io.loadmat(mat_file_path)
     except FileNotFoundError:
@@ -106,7 +108,50 @@ def K_means_classifier(x, C, c_labels):
     k_near = np.argmin(dist)
     return c_labels[k_near]
 
+def svm(X_train, y_train, X_test, C=1.0):
+    linearsvm = SVC(kernel='linear', C=C)
+    linearsvm.fit(X_train, y_train)
+    y_pred_train = linearsvm.predict(X_train)
+    y_pred_test  = linearsvm.predict(X_test)
+    return y_pred_train, y_pred_test
+
+def svm_data(y_true, y_pred):
+    n00 = np.sum((y_true==0) & (y_pred==0))
+    n01 = np.sum((y_true==1) & (y_pred==0))
+    n10 = np.sum((y_true==0) & (y_pred==1))
+    n11 = np.sum((y_true==1) & (y_pred==1))
+    return n00, n01, n10, n11
+    
+
+
 if __name__ == "__main__":
     data = load_data()
-    nbr_clusters = 4 # Replace with you chosen int
-    y, C = K_means_clustering(data[2], nbr_clusters)
+    
+    
+    # nbr_clusters = 4 # Replace with you chosen int
+    # y, C = K_means_clustering(data[2], nbr_clusters)
+    
+    #SVM (12)
+    y_pred_train, y_pred_test = svm(data[2].T, data[3].ravel(), data[0].T, C=1.0)
+    
+    n00_train, n01_train, n10_train, n11_train = svm_data(y_pred_train, y_pred_train)
+    N_train = y_pred_train.size
+    sum_train = n01_train + n10_train      # those got flipped
+    rate_train = sum_train / N_train * 100
+
+    n00_test , n01_test , n10_test , n11_test  = svm_data(y_pred_test,  y_pred_test)
+    N_test  = y_pred_test.size
+    sum_test  = n01_test  + n10_test
+    rate_test  = sum_test  / N_test  * 100
+    
+    print(f"N_train = {N_train}")
+    print(f"'0' {n00_train:6d} {n01_train:6d}")
+    print(f"'1' {n10_train:6d} {n11_train:6d}")
+    print(f"Sum misclassified = {sum_train}")
+    print(f"Misclassification rate (%) = {rate_train:.2f}%\n")
+    
+    print(f"N_test  = {N_test}")
+    print(f"'0' & {n00_test:6d} {n01_test:6d}")
+    print(f"'1' & {n10_test:6d} {n11_test:6d}")
+    print(f"Sum misclassified = {sum_test}")
+    print(f"Misclassification rate (%) = {rate_test:.2f}%")
